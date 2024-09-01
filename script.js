0d5cfb33eb2f2bef2f9d9cd5941b3fc704545575
heyvj <vijayalaxmi9915@gmail.com>
2024-09-01 14:55:58 -0700

    basic functionality of calc ready, all operators working

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ clearbutton.addEventListener("click", () => {
 
 const digits = document.querySelectorAll(".digit");
 const operators = document.querySelectorAll(".operator");
+const result = document.querySelector(".result");
 
 for (const dig of digits) {
   dig.addEventListener("click", () => {
@@ -24,4 +25,61 @@ for (const op of operators) {
   });
 }
 
-function main() {}
+function operate(num1, num2, operator) {
+  let result = null;
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      if (num2 == 0) result = "I told you not to divide by 0 :/";
+      result = num1 / num2;
+      break;
+  }
+
+  return result;
+}
+
+result.addEventListener("click", () => {
+  let inpStr = displaywindow.textContent;
+  let num1,
+    num2,
+    op,
+    output = null;
+  if (inpStr.includes("+")) {
+    let numarr = inpStr.split("+");
+    num1 = Number(numarr[0]);
+    num2 = Number(numarr[1]);
+    op = "+";
+    output = operate(num1, num2, op);
+    displaywindow.textContent = output;
+  } else if (inpStr.includes("-")) {
+    let numarr = inpStr.split("-");
+    num1 = Number(numarr[0]);
+    num2 = Number(numarr[1]);
+    op = "-";
+    output = operate(num1, num2, op);
+    displaywindow.textContent = output;
+  } else if (inpStr.includes("*")) {
+    let numarr = inpStr.split("*");
+    num1 = Number(numarr[0]);
+    num2 = Number(numarr[1]);
+    op = "*";
+    output = operate(num1, num2, op);
+    displaywindow.textContent = output;
+  } else if (inpStr.includes("/")) {
+    let numarr = inpStr.split("/");
+    num1 = Number(numarr[0]);
+    num2 = Number(numarr[1]);
+    op = "/";
+    output = operate(num1, num2, op);
+    displaywindow.textContent = output;
+  } else {
+  }
+});
